4a693a2d31fb88589b1d609a4c708e2017ccd6d5
rob-digital <robert.roksela@gmail.com>
2019-05-12 13:13:34 +0200

    first push

---
+++ app.js
@@ -0,0 +1,63 @@
+Vue.use(vuelidate.default)
+
+new Vue({
+  el: '#app',
+
+  data () {
+    return {
+      form: {
+        name: null,
+        age: null
+      }
+    }
+  },
+
+    
+  validations: {
+    form: {
+        name: {
+            required: validators.required
+        },
+        age: {
+            integer: validators.integer,
+            required: validators.required,
+            /*min: validators.minValue(12),
+            max: validators.maxValue(120)*/
+            between: validators.between(12, 120)
+        }
+        
+    }  
+      
+  },
+/* computed: {
+   nameIsValid () {
+       return !!this.form.name
+   },
+     
+   ageIsValid() {
+       return typeof this.form.age === 'number' && this.form.age > 6 && this.form.age < 121
+   },
+     
+    formIsValid() {
+        return this.nameIsValid && this.ageIsValid
+    }
+ },*/
+  
+  methods: {
+    submitForm () {
+       
+        
+            /*  if (this.formIsValid) {
+                console.log('📝 Form Submitted', this.form)
+              }else {
+                  console.log('Invalid form')
+              }*/
+        if (!this.$v.form.$invalid) {
+            console.log('Form submitted', this.form)
+        } else {
+            console.log('Invalid Form')
+        }
+      
+    }
+  }
+})
